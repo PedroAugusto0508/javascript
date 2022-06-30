da1bbd8f253f91ea149c313194ecddea3064d356
Pedro Augusto Lima Freitas <augustpedro286@gmail.com>
2022-06-30 16:31:33 -0300

    atualização de exercícios

---
+++ exercicios/ambiente2.js
@@ -0,0 +1,13 @@
+console.log('Vai começar...')
+for( var c = 1 ; c<=10  ; c++ ){
+    console.log(c)
+}
+console.log('FIM!')
+
+
+
+
+
+/*for( var c = 1 ; c<=10  ; c++ ){
+    console.log(c)
+}*/
---
+++ exercicios/ambiente3.js
@@ -0,0 +1,7 @@
+let num =[1, 3, 2 , 4, 5, 6, 7]
+
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
